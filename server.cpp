4da782a520aafa62148755a0283616e87ce51f0c
Kirill <113207598+YongKirill@users.noreply.github.com>
2023-10-17 15:59:23 +0300

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -14,41 +14,52 @@
 int main() { 
     int sockfd; 
     char buffer[MAXLINE]; 
-    std::string msg_to_send= "Hello from client";
-    struct sockaddr_in     servaddr; 
-   
+    std::string msg_to_send= "Hello from server";
+    struct sockaddr_in servaddr, cliaddr; 
+       
     // Creating socket file descriptor 
     if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
         perror("socket creation failed"); 
         exit(EXIT_FAILURE); 
     } 
-   
+       
     memset(&servaddr, 0, sizeof(servaddr)); 
+    memset(&cliaddr, 0, sizeof(cliaddr)); 
        
     // Filling server information 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(PORT); 
+    servaddr.sin_family    = AF_INET; // IPv4 
     servaddr.sin_addr.s_addr = INADDR_ANY; 
+    servaddr.sin_port = htons(PORT); 
        
-    int n;
-    socklen_t len; 
-    
+    // Bind the socket with the server address 
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
+            sizeof(servaddr)) < 0 ) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+       
+    socklen_t len;
+  int n; 
+   
+    len = sizeof(cliaddr);  //len is value/result 
     while(true)
     {
+        n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
+                    MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
+                    &len); 
+        buffer[n] = '\0'; 
+        printf("Client : %s\n", buffer); 
+        
         std::cout<<"Enter message:\t";
         std::cin>>msg_to_send;
         char* str_to_char = const_cast<char*>(msg_to_send.c_str());
-        sendto(sockfd, str_to_char, strlen(str_to_char), 
-            MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
-                sizeof(servaddr)); 
-            
-        n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
-                    MSG_WAITALL, (struct sockaddr *) &servaddr, 
-                    &len); 
-        buffer[n] = '\0'; 
-        std::cout<<"Server :"<<buffer<<std::endl; 
+        sendto(sockfd, str_to_char, strlen(str_to_char),  
+            MSG_CONFIRM, (const struct sockaddr *) &cliaddr, 
+                len); 
+
     }
-   
-    close(sockfd); 
+    
+       
     return 0; 
 }
