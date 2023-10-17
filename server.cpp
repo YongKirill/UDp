dfc5549616223669d1ae3634bc95ec2c5e57bf3c
Kirill <113207598+YongKirill@users.noreply.github.com>
2023-10-17 14:30:26 +0300

    Create server.cpp

---
+++ server.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+   
+#define PORT     8080 
+#define MAXLINE 1024 
+   
+// Driver code 
+int main() { 
+    int sockfd; 
+    char buffer[MAXLINE]; 
+    std::string msg_to_send= "Hello from client";
+    struct sockaddr_in     servaddr; 
+   
+    // Creating socket file descriptor 
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+   
+    memset(&servaddr, 0, sizeof(servaddr)); 
+       
+    // Filling server information 
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_port = htons(PORT); 
+    servaddr.sin_addr.s_addr = INADDR_ANY; 
+       
+    int n;
+    socklen_t len; 
+    
+    while(true)
+    {
+        std::cout<<"Enter message:\t";
+        std::cin>>msg_to_send;
+        char* str_to_char = const_cast<char*>(msg_to_send.c_str());
+        sendto(sockfd, str_to_char, strlen(str_to_char), 
+            MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
+                sizeof(servaddr)); 
+            
+        n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
+                    MSG_WAITALL, (struct sockaddr *) &servaddr, 
+                    &len); 
+        buffer[n] = '\0'; 
+        std::cout<<"Server :"<<buffer<<std::endl; 
+    }
+   
+    close(sockfd); 
+    return 0; 
+}
